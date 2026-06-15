8c14e618a6901c0db927e834029f0c53a835bae7
Sunkara810 <pavanisunkara52@gmail.com>
2026-06-15 12:31:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleTheme(){
+    document.body.classList.toggle("dark-mode");
+}
+
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+
+    sections.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+            section.style.opacity = "1";
+            section.style.transform = "translateY(0)";
+        }
+    });
+});
